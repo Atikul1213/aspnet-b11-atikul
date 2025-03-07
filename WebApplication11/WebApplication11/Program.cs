c38cc37f056c7fa261bf56049bb81c33b9cf71e8
Atikul1213 <atikul.islam@brainstation-23.com>
2025-03-07 23:05:20 +0600

    add ip correction

--- WebApplication11/WebApplication11/Program.cs
+++ WebApplication11/WebApplication11/Program.cs
@@ -14,6 +14,9 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
+#region Docker IP Corerection
+builder.WebHost.UseUrls("http://*:80");
+#endregion
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
