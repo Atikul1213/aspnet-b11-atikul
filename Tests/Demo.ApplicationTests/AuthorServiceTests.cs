49d4e93ffcf62ec8604e3aae8be69f1c678d52bf
Atikul1213 <atikul.islam@brainstation-23.com>
2025-05-03 17:21:40 +0600

    Add unt test solution

---
+++ Tests/Demo.ApplicationTests/AuthorServiceTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Demo.ApplicationTests
+{
+    public class AuthorServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void AddAuthor_UniqueName_AddsAuthor()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void AddAuthor_DuplicateName_ThrowsException()
+        {
+            Assert.Pass();
+        }
+    }
+}
