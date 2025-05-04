db3983a6a64f0b3de4447531cc38e6e65918c5f9
Atikul1213 <atikul.islam@brainstation-23.com>
2025-05-05 00:15:16 +0600

    add product repository signature

---
+++ DevSkill.Inventory.Web/DevSkill.Inventory.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace DevSkill.Inventory.Domain.Repositories
+{
+    public interface IProductRepository
+    {
+    }
+}
---
+++ DevSkill.Inventory.Web/DevSkill.Inventory.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using DevSkill.Inventory.Domain.Repositories;
+
+namespace DevSkill.Inventory.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+    }
+}
