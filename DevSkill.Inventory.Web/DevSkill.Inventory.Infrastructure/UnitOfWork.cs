7735ac03fe1a83852ad2a81676a7aff6a8e61e43
Atikul1213 <atikul.islam@brainstation-23.com>
2025-05-05 00:20:04 +0600

    implement iunit of work

--- DevSkill.Inventory.Web/DevSkill.Inventory.Infrastructure/UnitOfWork.cs
+++ DevSkill.Inventory.Web/DevSkill.Inventory.Infrastructure/UnitOfWork.cs
@@ -1,21 +1,23 @@
 ﻿using DevSkill.Inventory.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevSkill.Inventory.Infrastructure
 {
     public class UnitOfWork : IUnitOfWork
     {
-        public UnitOfWork()
+        private readonly DbContext _dbContext;
+        public UnitOfWork(DbContext dbContext)
         {
-
+            _dbContext = dbContext;
         }
         public void Save()
         {
-            throw new NotImplementedException();
+            _dbContext.SaveChanges();
         }
 
         public async Task SaveAsync()
         {
-            throw new NotImplementedException();
+            await _dbContext.SaveChangesAsync();
         }
     }
 }
