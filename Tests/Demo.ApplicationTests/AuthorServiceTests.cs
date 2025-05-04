4f7cab5d1426fc0bbcc3152ce4fd9fdfea9a8781
Atikul1213 <atikul.islam@brainstation-23.com>
2025-05-04 10:01:54 +0600

    add author service unit test

--- Tests/Demo.ApplicationTests/AuthorServiceTests.cs
+++ Tests/Demo.ApplicationTests/AuthorServiceTests.cs
@@ -1,22 +1,79 @@
-﻿namespace Demo.ApplicationTests
+﻿using Autofac.Extras.Moq;
+using Demo.Application.Services;
+using Demo.Domain;
+using Demo.Domain.Entities;
+using Demo.Domain.Repositories;
+using Demo.Domain.Services;
+using Moq;
+using Shouldly;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Demo.ApplicationTests
 {
+    [ExcludeFromCodeCoverage]
     public class AuthorServiceTests
     {
+        private AutoMock _moq;
+        private IAuthorService _authorService;
+        private Mock<IApplicationUnitOfWork> _applicationUnitOfWorkMock;
+        private Mock<IAuthorRepository> _authorRepositoryMock;
+
         [SetUp]
         public void Setup()
         {
+            _authorService = _moq.Create<AuthorService>();
+            _applicationUnitOfWorkMock = _moq.Mock<IApplicationUnitOfWork>();
+            _authorRepositoryMock = _moq.Mock<IAuthorRepository>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _applicationUnitOfWorkMock?.Reset();
+            _authorRepositoryMock?.Reset();
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            _moq = AutoMock.GetLoose();
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            _moq.Dispose();
         }
 
         [Test]
-        public void AddAuthor_UniqueName_AddsAuthor()
+        public async Task AddAuthor_UniqueName_AddsAuthor()
         {
-            Assert.Pass();
+            Author author = new Author()
+            {
+                Name = "Test Author",
+                Biography = "I am a test author.",
+                Rating = 3.4
+            };
+
+            _applicationUnitOfWorkMock.SetupGet(x => x.AuthorRepository).Returns(_authorRepositoryMock.Object);
+
+            _authorRepositoryMock.Setup(x => x.IsNameDuplicate(author.Name, null)).Returns(false).Verifiable();
+            _authorRepositoryMock.Setup(x => x.Add(author)).Verifiable();
+            _applicationUnitOfWorkMock.Setup(x => x.Save()).Verifiable();
+
+            _authorService.AddAuthor(author);
+
+            this.ShouldSatisfyAllConditions(
+                _applicationUnitOfWorkMock.VerifyAll,
+                _authorRepositoryMock.VerifyAll
+            );
         }
+
+
 
         [Test]
         public void AddAuthor_DuplicateName_ThrowsException()
         {
-            Assert.Pass();
         }
     }
 }
