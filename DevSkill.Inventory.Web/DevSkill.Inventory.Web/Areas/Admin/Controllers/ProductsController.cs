85e01f757b31aa9c22d0eff61acdc732eaf5f259
Atikul1213 <atikul.islam@brainstation-23.com>
2025-05-05 21:48:02 +0600

    add product controller

---
+++ DevSkill.Inventory.Web/DevSkill.Inventory.Web/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevSkill.Inventory.Web.Areas.Admin.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
