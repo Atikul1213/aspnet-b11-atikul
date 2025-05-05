2afbd15b0cb69f023d58ffd656b5d32c6a7c9eef
Atikul1213 <atikul.islam@brainstation-23.com>
2025-05-05 21:53:22 +0600

    add product create action

--- DevSkill.Inventory.Web/DevSkill.Inventory.Web/Areas/Admin/Controllers/ProductsController.cs
+++ DevSkill.Inventory.Web/DevSkill.Inventory.Web/Areas/Admin/Controllers/ProductsController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevSkill.Inventory.Web.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevSkill.Inventory.Web.Areas.Admin.Controllers
 {
     public class ProductsController : Controller
     {
+        #region Fields
+
+        #endregion
+
+        #region Ctor
+        public ProductsController()
+        {
+
+        }
+        #endregion
+
+        #region Methods
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult Create()
+        {
+            var model = new AddProductModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(AddProductModel mode)
+        {
+
+
+            return View(mode);
+        }
+        #endregion
     }
 }
--- DevSkill.Inventory.Web/DevSkill.Inventory.Web/Areas/Admin/Models/AddProductModel.cs
+++ DevSkill.Inventory.Web/DevSkill.Inventory.Web/Areas/Admin/Models/AddProductModel.cs
@@ -2,5 +2,9 @@
 {
     public class AddProductModel
     {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public bool IsAvailable { get; set; }
     }
 }
