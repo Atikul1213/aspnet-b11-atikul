e29e570ebf0bc2df92e7865c1bb8bf28ca3e3245
Atikul1213 <atikul.islam@brainstation-23.com>
2025-05-07 21:47:38 +0600

    add sku column in product

--- DevSkill.Inventory.Web/DevSkill.Inventory.Domain/Entities/Product.cs
+++ DevSkill.Inventory.Web/DevSkill.Inventory.Domain/Entities/Product.cs
@@ -4,6 +4,7 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public string Sku { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
         public bool IsAvailable { get; set; }
--- DevSkill.Inventory.Web/DevSkill.Inventory.Web/Areas/Admin/Models/Products/AddProductModel.cs
+++ DevSkill.Inventory.Web/DevSkill.Inventory.Web/Areas/Admin/Models/Products/AddProductModel.cs
@@ -6,5 +6,6 @@
         public decimal Price { get; set; }
         public int Quantity { get; set; }
         public bool IsAvailable { get; set; }
+        public string Sku { get; set; }
     }
 }
