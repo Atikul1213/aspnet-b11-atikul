8a76ff7a209a35d9568be9e322687b8e85335e0c
Atikul1213 <atikul.islam@brainstation-23.com>
2025-06-04 09:50:20 +0600

    Add unit and product category

---
+++ DevSkill.Inventory.Web/DevSkill.Inventory.Domain/Entities/Category.cs
@@ -0,0 +1,21 @@
+﻿namespace DevSkill.Inventory.Domain.Entities
+{
+    public enum Category
+    {
+        Electronics = 1,
+        Clothing = 2,
+        Groceries = 3,
+        Furniture = 4,
+        Books = 5,
+        Toys = 6,
+        Sports = 7,
+        HealthAndBeauty = 8,
+        Automotive = 9,
+        HomeAppliances = 10,
+        Jewelry = 11,
+        MusicalInstruments = 12,
+        ToysGames = 13,
+        Laptops = 14,
+        FoodAndBeverages = 15,
+    }
+}
---
+++ DevSkill.Inventory.Web/DevSkill.Inventory.Domain/Entities/ProductUnit.cs
@@ -0,0 +1,18 @@
+﻿namespace DevSkill.Inventory.Domain.Entities
+{
+    public enum ProductUnit
+    {
+        Piece = 1,
+        Kilogram = 2,
+        Gram = 3,
+        Liter = 4,
+        Milliliter = 5,
+        Meter = 6,
+        Centimeter = 7,
+        Inch = 8,
+        Gallon = 9,
+        Dozen = 10,
+        Box = 11,
+        Bundle = 12
+    }
+}
