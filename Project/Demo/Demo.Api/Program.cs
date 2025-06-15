111df79af303e7c4f7d9e33eab0cc561be4d0163
Atikul1213 <atikul.islam@brainstation-23.com>
2025-06-15 20:05:54 +0600

    add web api solution

---
+++ Project/Demo/Demo.Api/Program.cs
@@ -0,0 +1,76 @@
+
+#region BootStrap Logger
+
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Demo.Api;
+using Serilog;
+using Serilog.Events;
+using System.Reflection;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateBootstrapLogger();
+#endregion
+
+try
+{
+    Log.Information("Application is starting...");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    // Add services to the container.
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+    var migrationAssembly = Assembly.GetExecutingAssembly();
+
+    #region Autofac Configuration
+    builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+    builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+    {
+        containerBuilder.RegisterModule(new ApiModule(connectionString, migrationAssembly?.FullName));
+    });
+    #endregion
+
+    #region Serilog Configuration
+    builder.Host.UseSerilog((context, lc) => lc
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .ReadFrom.Configuration(builder.Configuration)
+            );
+    #endregion
+
+    builder.Services.AddControllers();
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    builder.Services.AddOpenApi();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application failed to start");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
