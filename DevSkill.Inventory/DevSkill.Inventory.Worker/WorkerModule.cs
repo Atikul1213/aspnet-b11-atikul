85899c53526504cb70c9963aefe8c33f206be178
Atikul1213 <atikul.islam@brainstation-23.com>
2025-06-24 23:17:03 +0600

    add worker service

---
+++ DevSkill.Inventory/DevSkill.Inventory.Worker/WorkerModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+
+namespace DevSkill.Inventory.Worker
+{
+    public class WorkerModule : Module
+    {
+        private readonly string _connectionString;
+        private readonly string _migrationAssemblyName;
+        public WorkerModule(string connectionString, string migrationAssemblyName)
+        {
+            _connectionString = connectionString;
+            _migrationAssemblyName = migrationAssemblyName;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            base.Load(builder);
+        }
+    }
+}
